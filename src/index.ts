36d0f6ec6251bb67c2f333ca4907a89ca057a139
Ivan <ivanms1@gmail.com>
2020-01-17 00:08:32 -0300

    cors change

--- src/index.ts
+++ src/index.ts
@@ -41,7 +41,7 @@ const app = express();
 app.use(cookieParser());
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://nuzlocketracker.netlify.com',
     credentials: true
   })
 );
